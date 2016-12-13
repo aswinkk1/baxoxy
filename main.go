d6cc8f7369f59d50a2a524d34fbb04911f095aeb
aswin-qb <aswinb@qburst.com>
2016-12-13 12:31:35 +0530

    [#11]Changed responses to JSON format Changed Login and Signup responses to JSON format Added token in response to Login

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
-import(
-  "github.com/aswinkk1/baxoxy/server"
+import (
+	"github.com/aswinkk1/baxoxy/server"
 )
 
 func main() {
-  server.CreateServer()
+	server.CreateServer()
 }
--- server/server.go
+++ server/server.go
@@ -2,57 +2,55 @@ package server
 
 import (
 	// Third party packages
-	"github.com/kataras/iris"
-	"github.com/dgrijalva/jwt-go"
 	"github.com/aswinkk1/baxoxy/controllers"
-	 jwtmiddleware "github.com/iris-contrib/middleware/jwt"
+	"github.com/dgrijalva/jwt-go"
+	jwtmiddleware "github.com/iris-contrib/middleware/jwt"
+	"github.com/kataras/iris"
 	"gopkg.in/mgo.v2"
 )
-
 
 func CreateServer() {
 
 	// Get a UserController instance
 	uc := controllers.NewUserController(getSession())
-	
+
 	myJwtMiddleware := jwtmiddleware.New(jwtmiddleware.Config{
-        ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
-            return []byte("secret"), nil
-        },
-        SigningMethod: jwt.SigningMethodHS256,
-    })
+		ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
+			return []byte("secret"), nil
+		},
+		SigningMethod: jwt.SigningMethodHS256,
+	})
 	// Create a new user
 	iris.Post("webchat/signup", uc.CreateUser)
-	
+
 	//login
 	iris.Post("webchat/login", uc.Login)
-	
+
 	//test
 	iris.Post("webchat/signin", myJwtMiddleware.Serve, uc.SecuredPingHandler)
-	
 
-//	// Remove an existing user
-//	iris.DELETE("webchat/users/:id", uc.RemoveUser)
+	//	// Remove an existing user
+	//	iris.DELETE("webchat/users/:id", uc.RemoveUser)
 	iris.OnError(iris.StatusInternalServerError, func(ctx *iris.Context) {
-        ctx.Write("CUSTOM 500 INTERNAL SERVER ERROR PAGE")
-        // or ctx.Render, ctx.HTML any render method you want
-        ctx.Log("http status: 500 happened!")
-    })
+		ctx.Write("CUSTOM 500 INTERNAL SERVER ERROR PAGE")
+		// or ctx.Render, ctx.HTML any render method you want
+		ctx.Log("http status: 500 happened!")
+	})
 
-    iris.OnError(iris.StatusNotFound, func(ctx *iris.Context) {
-        ctx.Write("CUSTOM 404 NOT FOUND ERROR PAGE")
-        ctx.Log("http status: 404 happened!")
-    })
+	iris.OnError(iris.StatusNotFound, func(ctx *iris.Context) {
+		ctx.Write("CUSTOM 404 NOT FOUND ERROR PAGE")
+		ctx.Log("http status: 404 happened!")
+	})
 
-    // emit the errors to test them
-    iris.Get("/500", func(ctx *iris.Context) {
-        ctx.EmitError(iris.StatusInternalServerError) // ctx.Panic()
-    })
+	// emit the errors to test them
+	iris.Get("/500", func(ctx *iris.Context) {
+		ctx.EmitError(iris.StatusInternalServerError) // ctx.Panic()
+	})
 
-    iris.Get("/404", func(ctx *iris.Context) {
-        ctx.EmitError(iris.StatusNotFound) // ctx.NotFound()
-    })
-	
+	iris.Get("/404", func(ctx *iris.Context) {
+		ctx.EmitError(iris.StatusNotFound) // ctx.NotFound()
+	})
+
 	// Fire up the server
 	iris.Listen("localhost:5700")
 }
