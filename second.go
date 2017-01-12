17dc3db326e1348a952b2550a04498f5996d8e9b
jaseel <connectjaseel001@gmail.com>
2017-01-12 09:37:22 +0530

    Two servers

--- second.go
+++ second.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main() {
-	server.CreateServer2()
+	server.CreateServer()
 }
--- server/server.go
+++ server/server.go
@@ -2,9 +2,12 @@ package server
 
 import (
 	"log"
-	"strings"
-	"encoding/json"
+	//"strings"
+	//"encoding/json"
 	"time"
+	"bytes"
+	"flag"
+	"net/http"
 	// Third party packages
 	"github.com/aswinkk1/baxoxy/controllers"
 	//"github.com/davecgh/go-spew/spew"
@@ -12,6 +15,8 @@ import (
 	jwtmiddleware "github.com/iris-contrib/middleware/jwt"
 	"github.com/kataras/iris"
 	"gopkg.in/mgo.v2"
+	"github.com/gorilla/websocket"
+
 )
 
 var ActiveClients = make(map[string]string)
@@ -32,6 +37,8 @@ type Datas struct {
 	To string `json:"to"`
 	Author string `json:"author"`
 }
+
+var addr = flag.String("addr", ":8080", "http service address")
 
 func CreateServer() {
 
@@ -59,48 +66,7 @@ func CreateServer() {
 	//test
 	iris.Post("webchat/signin", myJwtMiddleware.Serve, uc.SecuredPingHandler)
 
-	iris.Config.Websocket.Endpoint = "/"
-    // for Allow origin you can make use of the middleware
-    //iris.Config.Websocket.Headers["Access-Control-Allow-Origin"] = "*"
-	 //var myChatRoom = "room1"
-    iris.Websocket.OnConnection(func(c iris.WebsocketConnection) {
-    	id := c.ID()
-    	log.Println("\nConnection with ID: %s has been connected!", id)
-		token := strings.TrimPrefix(c.Request().RequestURI,"/?token=")
-		if username,err := TokenParser(token); err == nil{
-			ActiveClients[username] = id
-			log.Println(ActiveClients)
-		}else{
-			go c.Disconnect();
-			//log.Println("er",er)
-			log.Println("err",err)
-		}
-		//spew.Dump(c)
-        //c.Join(myChatRoom)
-        c.OnMessage(func(message []byte){
-        	var msg Message
-        	log.Println(string(message))
-        	if err := json.Unmarshal(message, &msg); err != nil {
-        		panic(err)
-    		}
-    		token := strings.TrimPrefix(c.Request().RequestURI,"/?token=")
-    		username,_ := TokenParser(token)
-    		t := time.Now()
-    		var dat = Datas{Time:t.Format("2006/01/02/15:04:05"),Text:msg.Msg,To:msg.To,Author:username}
-			var rep = Reply{Type:"message",Data: dat}
-			b, _ := json.Marshal(rep)
-    		c.To(ActiveClients[msg.To]).EmitMessage(b)
-    		c.EmitMessage(b)
-        })
 
-        c.OnDisconnect(func() {
-        	token := strings.TrimPrefix(c.Request().RequestURI,"/?token=")
-        	log.Println(token)
-        	username,_ := TokenParser(token)
-        	delete(ActiveClients,username)
-            log.Println("\nConnection with ID: %s has been disconnected!", c.ID())
-        })
-    })
 	//	// Remove an existing user
 	//	iris.DELETE("webchat/users/:id", uc.RemoveUser)
 	iris.OnError(iris.StatusInternalServerError, func(ctx *iris.Context) {
@@ -125,8 +91,22 @@ func CreateServer() {
 
 	// Fire up the server
 	iris.Listen("10.7.20.26:5700")
+
 }
 
+func CreateServer2() {
+	flag.Parse()
+	hub := newHub()
+	go hub.run()
+	//http.HandleFunc("/", serveHome)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(hub, w, r)
+	})
+	err := http.ListenAndServe(*addr, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
 // getSession creates a new mongo session and panics if connection error occurs
 func getSession() *mgo.Session {
 	// Connect to our local mongo
@@ -161,4 +141,192 @@ func TokenParser(myToken string) (string,error){
         log.Println(err)
     }
     return username,err
+}
+/*
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL)
+	if r.URL.Path != "/" {
+		http.Error(w, "Not found", 404)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", 405)
+		return
+	}
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+}*/
+
+
+//hub
+
+type Hub struct {
+	// Registered clients.
+	clients map[*Client]bool
+
+	// Inbound messages from the clients.
+	broadcast chan []byte
+
+	// Register requests from the clients.
+	register chan *Client
+
+	// Unregister requests from clients.
+	unregister chan *Client
+}
+
+func newHub() *Hub {
+	return &Hub{
+		broadcast:  make(chan []byte),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+		clients:    make(map[*Client]bool),
+	}
+}
+
+func (h *Hub) run() {
+	for {
+		select {
+		case client := <-h.register:
+			h.clients[client] = true
+		case client := <-h.unregister:
+			if _, ok := h.clients[client]; ok {
+				delete(h.clients, client)
+				close(client.send)
+			}
+		case message := <-h.broadcast:
+			for client := range h.clients {
+				select {
+				case client.send <- message:
+				default:
+					close(client.send)
+					delete(h.clients, client)
+				}
+			}
+		}
+	}
+}
+
+
+
+//client
+
+
+const (
+	// Time allowed to write a message to the peer.
+	writeWait = 10 * time.Second
+
+	// Time allowed to read the next pong message from the peer.
+	pongWait = 60 * time.Second
+
+	// Send pings to peer with this period. Must be less than pongWait.
+	pingPeriod = (pongWait * 9) / 10
+
+	// Maximum message size allowed from peer.
+	maxMessageSize = 512
+)
+
+var (
+	newline = []byte{'\n'}
+	space   = []byte{' '}
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
+
+// Client is a middleman between the websocket connection and the hub.
+type Client struct {
+	hub *Hub
+
+	// The websocket connection.
+	conn *websocket.Conn
+
+	// Buffered channel of outbound messages.
+	send chan []byte
+}
+
+// readPump pumps messages from the websocket connection to the hub.
+//
+// The application runs readPump in a per-connection goroutine. The application
+// ensures that there is at most one reader on a connection by executing all
+// reads from this goroutine.
+func (c *Client) readPump() {
+	defer func() {
+		c.hub.unregister <- c
+		c.conn.Close()
+	}()
+	c.conn.SetReadLimit(maxMessageSize)
+	c.conn.SetReadDeadline(time.Now().Add(pongWait))
+	c.conn.SetPongHandler(func(string) error { c.conn.SetReadDeadline(time.Now().Add(pongWait)); return nil })
+	for {
+		_, message, err := c.conn.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway) {
+				log.Printf("error: %v", err)
+			}
+			break
+		}
+		message = bytes.TrimSpace(bytes.Replace(message, newline, space, -1))
+		c.hub.broadcast <- message
+	}
+}
+
+// writePump pumps messages from the hub to the websocket connection.
+//
+// A goroutine running writePump is started for each connection. The
+// application ensures that there is at most one writer to a connection by
+// executing all writes from this goroutine.
+func (c *Client) writePump() {
+	ticker := time.NewTicker(pingPeriod)
+	defer func() {
+		ticker.Stop()
+		c.conn.Close()
+	}()
+	for {
+		select {
+		case message, ok := <-c.send:
+			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if !ok {
+				// The hub closed the channel.
+				c.conn.WriteMessage(websocket.CloseMessage, []byte{})
+				return
+			}
+
+			w, err := c.conn.NextWriter(websocket.TextMessage)
+			if err != nil {
+				return
+			}
+			w.Write(message)
+
+			// Add queued chat messages to the current websocket message.
+			n := len(c.send)
+			for i := 0; i < n; i++ {
+				w.Write(newline)
+				w.Write(<-c.send)
+			}
+
+			if err := w.Close(); err != nil {
+				return
+			}
+		case <-ticker.C:
+			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if err := c.conn.WriteMessage(websocket.PingMessage, []byte{}); err != nil {
+				return
+			}
+		}
+	}
+}
+
+// serveWs handles websocket requests from the peer.
+func serveWs(hub *Hub, w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	client := &Client{hub: hub, conn: conn, send: make(chan []byte, 256)}
+	client.hub.register <- client
+	go client.writePump()
+	client.readPump()
 }
